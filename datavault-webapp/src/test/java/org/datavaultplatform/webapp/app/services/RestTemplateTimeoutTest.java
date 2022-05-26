3c7f6f9e574576f62e21c519433a874b110f4a1b
David Hay <david.j.hay@gmail.com>
2022-05-26 14:00:35 +0100

    dv-5 RSS408-164 - upgrade to spring-boot 2.7.0

--- datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/services/RestTemplateTimeoutTest.java
+++ datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/services/RestTemplateTimeoutTest.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.client.AbstractClientHttpRequestFactoryWrapper;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
